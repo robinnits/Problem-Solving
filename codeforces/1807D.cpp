aa6c1aaf3cd4f6573acd62f7fbe989e4a716c547
Robin Poddar Partha <robinpoddar2003@gmail.com>
2025-08-07 08:08:44 +0530

    Odd queries

--- codeforces/1807D.cpp
+++ codeforces/1807D.cpp
@@ -11,13 +11,21 @@ int main() {
         int odd=0, even = 0;
         for(int i = 0; i<n; i++) {
             cin>>arr[i];
-            if(arr[i]%2!=0) odd++;
+        }
+        int sums[n];
+        sums[0] = arr[0];
+        for(int i = 1; i<n; i++) {
+            sums[i] = arr[i] +sums[i-1];
         }
         while(q--) {
             int sum = 0;
             int l, r, k;
             cin>>l>>r>>k;
-            sum = sum + k*(l-r+1);
+            if(l>=2) {
+                sum = sums[l-2] + k*(r-l+1) + (sums[n-1]-sums[r-1]);
+            }else {
+                sum = k*(r-l+1) + (sums[n-1]-sums[r-1]);
+            }
             if(sum%2==0) cout<<"NO\n";
             else cout<<"YES\n";
         }
